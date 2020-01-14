45576cafef0456984081ac57d5cfeaaa976edd89
dingxiaowei <1213250243@qq.com>
2020-01-14 15:47:58 +0800

    提升CPU的计算速度

---
+++ Assets/Scripts/JobSystem/Testing.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Jobs;
+using Unity.Collections;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField]
+    private bool useJobs;
+    [SerializeField]
+    private int count = 10;
+
+    void Update()
+    {
+        float startTime = Time.realtimeSinceStartup;
+
+        if (useJobs)
+        {
+            NativeList<JobHandle> jobHandleList = new NativeList<JobHandle>(Allocator.Temp);
+            for (int i = 0; i < count; i++)
+            {
+                var jobHandle = ReallyToughTaskJob();
+                jobHandleList.Add(jobHandle);
+                //jobHandle.Complete();
+            }
+            JobHandle.CompleteAll(jobHandleList);
+            jobHandleList.Dispose();
+        }
+        else
+        {
+            for (int i = 0; i < count; i++)
+            {
+                ReallyToughTask();
+            }
+        }
+        Debug.Log(((Time.realtimeSinceStartup - startTime) * 1000f) + "ms");
+    }
+    private void ReallyToughTask()
+    {
+        float value = 0f;
+        for (int i = 0; i < 50000; i++)
+        {
+            value = math.exp10(math.sqrt(value));
+        }
+    }
+
+    private JobHandle ReallyToughTaskJob()
+    {
+        var job = new ReallyToughJob();
+        return job.Schedule();
+    }
+}
+
+public struct ReallyToughJob : IJob
+{
+    //public float something;
+    public void Execute()
+    {
+        float value = 0f;
+        for (int i = 0; i < 50000; i++)
+        {
+            value = math.exp10(math.sqrt(value));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # Unity_ECS_HelloWorld
-## Unity_ECS 入门
+## Unity_ECS提升游戏的运行效率
 
 ## 添加组件
 * Entities
 * Mathematics
 * Hybrid Renderer
+* Jobs
 
 ## 效果图对比
-用ECS创建10000个cube移动和传统方式创建10000cube移动帧率会有明显差异
-![](1.png)
-![](2.png)
+1.用ECS创建10000个cube移动和传统方式创建10000cube移动帧率会有明显差异
+![](效果图/1.png)
+![](效果图/2.png)
+
+2.利用Job System提高CPU的运算速度
+![](效果图/3.png)
+![](效果图/4.png)
+可以看出利用JobSystem计算速度提升了10倍左右
