83dab9ffb75134743f14220a0a1fc3b5a7797453
dingxiaowei <1213250243@qq.com>
2020-01-14 18:11:11 +0800

    案例3

--- Assets/Scripts/JobSystem/Testing.cs
+++ Assets/Scripts/JobSystem/Testing.cs
@@ -12,11 +12,81 @@ public class Testing : MonoBehaviour
     private bool useJobs;
     [SerializeField]
     private int count = 10;
+    [SerializeField]
+    private int zombieCount = 1000;
+    [SerializeField]
+    private Transform pfZombie;
+    private List<Zombie> zombieList;
+
+    public class Zombie
+    {
+        public Transform transform;
+        public float moveY;
+    }
+
+    private void Start()
+    {
+        zombieList = new List<Zombie>();
+        for (int i = 0; i < zombieCount; i++)
+        {
+            Transform zombieTransform = Instantiate(pfZombie, new Vector3(UnityEngine.Random.Range(-8f, 8f), UnityEngine.Random.Range(-5f, 5f)), Quaternion.identity);
+            zombieList.Add(new Zombie
+            {
+                transform = zombieTransform,
+                moveY = UnityEngine.Random.Range(1f, 2f)
+            });
+        }
+    }
 
     void Update()
     {
         float startTime = Time.realtimeSinceStartup;
+        if (useJobs)
+        {
+            NativeArray<float3> positionArray = new NativeArray<float3>(zombieList.Count, Allocator.TempJob);
+            NativeArray<float> moveYArray = new NativeArray<float>(zombieList.Count, Allocator.TempJob);
 
+            for (int i = 0; i < zombieList.Count; i++)
+            {
+                positionArray[i] = zombieList[i].transform.position;
+                moveYArray[i] = zombieList[i].moveY;
+            }
+
+            var reallyToughParallelJob = new ReallyTouchParallelJob
+            {
+                deltime = Time.deltaTime,
+                positionArray = positionArray,
+                moveYArray = moveYArray
+            };
+
+            var jobHandle = reallyToughParallelJob.Schedule(zombieList.Count, 100);
+            jobHandle.Complete();
+
+            for (int i = 0; i < zombieList.Count; i++)
+            {
+                zombieList[i].transform.position = positionArray[i];
+                zombieList[i].moveY = moveYArray[i];
+            }
+
+            positionArray.Dispose();
+            moveYArray.Dispose();
+        }
+        else
+        {
+            foreach (var zombie in zombieList)
+            {
+                zombie.transform.position += new Vector3(0, zombie.moveY * Time.deltaTime);
+                if (zombie.transform.position.y > 5f)
+                {
+                    zombie.moveY = -math.abs(zombie.moveY);
+                }
+                if (zombie.transform.position.y < -5f)
+                {
+                    zombie.moveY += math.abs(zombie.moveY);
+                }
+            }
+        }
+        /*
         if (useJobs)
         {
             NativeList<JobHandle> jobHandleList = new NativeList<JobHandle>(Allocator.Temp);
@@ -36,6 +106,7 @@ public class Testing : MonoBehaviour
                 ReallyToughTask();
             }
         }
+        */
         Debug.Log(((Time.realtimeSinceStartup - startTime) * 1000f) + "ms");
     }
     private void ReallyToughTask()
@@ -64,5 +135,32 @@ public struct ReallyToughJob : IJob
         {
             value = math.exp10(math.sqrt(value));
         }
+    }
+}
+
+
+public struct ReallyTouchParallelJob : IJobParallelFor
+{
+    public NativeArray<float3> positionArray;
+    public NativeArray<float> moveYArray;
+    [ReadOnly]
+    public float deltime;
+
+    public void Execute(int index)
+    {
+        positionArray[index] += new float3(0, moveYArray[index] * deltime, 0f);
+        if (positionArray[index].y > 5f)
+        {
+            moveYArray[index] = -math.abs(moveYArray[index]);
+        }
+        if (positionArray[index].y < -5f)
+        {
+            moveYArray[index] += math.abs(moveYArray[index]);
+        }
+        //float value = 0f;
+        //for (int i = 0; i < 1000; i++)
+        //{
+        //    value = math.exp10(math.sqrt(value));
+        //}
     }
 }
